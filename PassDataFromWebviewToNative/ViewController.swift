7ee3539e27ec0c144f6b01ebfbb77733f4ca04b2
NRintaro <hemisphere.babylon@icloud.com>
2018-06-27 11:02:41 +0900

    Initial Commit

---
+++ PassDataFromWebviewToNative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataFromWebviewToNative
+//
+//  Created by 中村麟太郎 on 2018/06/27.
+//  Copyright © 2018年 中村麟太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
