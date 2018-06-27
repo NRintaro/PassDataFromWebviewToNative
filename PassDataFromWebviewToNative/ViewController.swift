50d7e9b53ea07fbe6a93bf1fcc8022ebeaeeb415
NRintaro <hemisphere.babylon@icloud.com>
2018-06-27 12:06:42 +0900

    WKWebViewでWebViewからネイティブへデータを受け取る方法

--- PassDataFromWebviewToNative/ViewController.swift
+++ PassDataFromWebviewToNative/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+
+  var webView: WKWebView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // javaScriptを呼び出し可能にするWKUserContentControllerクラスの生成
+    let userController = WKUserContentController()
+    userController.add(self, name: "callbackHandler")
+
+    // WKWebViewの設定を行う為のWKWebViewConfigurationクラスの生成
+    let webConfiguration = WKWebViewConfiguration()
+    webConfiguration.userContentController = userController
+
+    // WKWebViewの生成
+    webView = WKWebView(frame: CGRect(x: 0,
+                                      y: 0,
+                                  width: self.view.frame.size.width,
+                                 height: self.view.frame.size.height),
+                configuration: webConfiguration)
+
+    webView.navigationDelegate = self
+    webView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(webView)
+
+    // 表示するアドレスを設定
+    let request = URLRequest(url: URL(string: "http://localhost:8080")!)
+    self.webView.load(request)
+  }
+
+  // WKScriptMessageHandlerプロトコル
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    if (message.name == "callbackHandler") {
+      print(message.body)
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
-
